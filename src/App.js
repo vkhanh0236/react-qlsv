5e293c6175749dd15183dc5a2d38b2cf0c9eb41b
vkhanh0236 <vkhanh0236@mail.com>
2021-03-31 20:19:22 +0700

    udate

--- src/App.js
+++ src/App.js
@@ -68,6 +68,7 @@ function App() {
           closeModalEdit={closeModalEdit}
           showModalEdit={showModalEdit}
           Id={Id}
+          students={students}
         />
       )}
     </div>
--- src/components/modalEdit.js
+++ src/components/modalEdit.js
@@ -1,37 +1,47 @@
 import { Button, Modal, Form } from "react-bootstrap";
-export default function ModalEdit({ showModalEdit, Id, closeModalEdit }) {
-  const saveEdit = (Id) => {
-      console.log(Id)
+export default function ModalEdit({
+  showModalEdit,
+  Id,
+  closeModalEdit,
+  students,
+}) {
+  const saveEdit = () => {
+    console.log(Id);
   };
+  const profile = students.filter((student) => student.id === Id);
+  const newww = profile.map((profile) => (
+    <Modal.Body key={profile.id}>
+      <Form.Group controlId="formBasicName">
+        <Form.Label>Name</Form.Label>
+        <Form.Control type="text" placeholder={profile.name} value={profile.name} />
+      </Form.Group>
+      <Form.Group controlId="formBasicBirthday">
+        <Form.Label>Birthday</Form.Label>
+        <Form.Control type="text" placeholder={profile.birthday} value={profile.birthday} />
+      </Form.Group>
+      <Form.Group controlId="formBasicEmail">
+        <Form.Label>Email</Form.Label>
+        <Form.Control type="email" placeholder={profile.email} value={profile.email} />
+      </Form.Group>
+      <Form.Group controlId="formBasicPhone">
+        <Form.Label>Phone</Form.Label>
+        <Form.Control type="text" placeholder={profile.phone} value={profile.phone} />
+      </Form.Group>
+    </Modal.Body>
+  ));
+  console.log(newww);
   return (
     <>
       <Modal show={showModalEdit} onHide={closeModalEdit}>
         <Modal.Header closeButton>
           <Modal.Title>Edit student</Modal.Title>
         </Modal.Header>
-        <Modal.Body>
-          <Form.Group controlId="formBasicName">
-            <Form.Label>Name</Form.Label>
-            <Form.Control type="text" placeholder="Name" />
-          </Form.Group>
-          <Form.Group controlId="formBasicBirthday">
-            <Form.Label>Birthday</Form.Label>
-            <Form.Control type="text" placeholder="Birthday" />
-          </Form.Group>
-          <Form.Group controlId="formBasicEmail">
-            <Form.Label>Email</Form.Label>
-            <Form.Control type="email" placeholder="Email" />
-          </Form.Group>
-          <Form.Group controlId="formBasicPhone">
-            <Form.Label>Phone</Form.Label>
-            <Form.Control type="number" placeholder="Phone" />
-          </Form.Group>
-        </Modal.Body>
+        {newww}
         <Modal.Footer>
           <Button variant="secondary" onClick={closeModalEdit}>
             Close
           </Button>
-          <Button variant="primary" onClick={() => saveEdit(Id)}>
+          <Button variant="primary" onClick={() => saveEdit()}>
             Save Changes
           </Button>
         </Modal.Footer>
